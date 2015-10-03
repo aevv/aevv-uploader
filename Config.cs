e4018f78d00bdd1a3d6c0cb41506ae15ef5a90de
Matt Thompson <matt@aevv.net>
2015-10-03 19:13:09 +0100

    Setup config object

---
+++ Config.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace aevvuploader
+{
+    class Config
+    {
+        public string ApiKey { get; set; }
+
+        public void Save(string fileName)
+        {
+            using (var writer = new StreamWriter(fileName, false))
+            {
+                writer.Write(JsonConvert.SerializeObject(this));
+            }
+        }
+
+        public static Config Load(string fileName)
+        {
+            if (!File.Exists(fileName))
+                return new Config();
+
+            return JsonConvert.DeserializeObject<Config>(File.ReadAllText(fileName));
+        }
+    }
+}
